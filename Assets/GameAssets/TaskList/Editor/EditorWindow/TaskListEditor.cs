dabc04b5a70f007997781e25ee4ca72388e139ea
Alan Stelmach <alan.stelmach1@gmail.com>
2021-12-06 01:03:14 +0100

    Connecting UI elements with in code objects, adding click method to button

--- Assets/GameAssets/TaskList/Editor/EditorWindow/TaskListEditor.cs
+++ Assets/GameAssets/TaskList/Editor/EditorWindow/TaskListEditor.cs
@@ -8,6 +8,10 @@ using UnityEditor.UIElements;
 public class TaskListEditor : EditorWindow
 {
     VisualElement container;
+    TextField taskText;
+    Button addTaskButton;
+    ScrollView taskListScrollView;
+    public const string path = "Assets/GameAssets/TaskList/Editor/EditorWindow/";
     [MenuItem("GameAssets/Task List")]
     public static void ShowWindow()
     {
@@ -20,10 +24,25 @@ public class TaskListEditor : EditorWindow
     {
         container = rootVisualElement;
 
-        VisualTreeAsset visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/GameAssets/TaskList/Editor/EditorWindow/TaskListEditor.uxml");
+        VisualTreeAsset visualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path + "TaskListEditor.uxml");
         container.Add(visualTreeAsset.Instantiate()); // Adding uxml template
 
-        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/GameAssets/TaskList/Editor/EditorWindow/TaskListEditor.uss");
+        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(path + "TaskListEditor.uss");
         container.styleSheets.Add(styleSheet); // Adding uss styles
+
+        taskText = container.Q<TextField>("taskText"); // Connecting UI elements with code objects
+        addTaskButton = container.Q<Button>("addTaskButton");
+        taskListScrollView = container.Q<ScrollView>("taskList");
+
+        // Debug.Log(taskText); // Logs
+        // Debug.Log(addTaskButton);
+        // Debug.Log(taskListScrollView);
+
+        addTaskButton.clicked += AddTask;
+    }
+
+    public void AddTask()
+    {
+        Debug.Log("Task Added");
     }
 }
